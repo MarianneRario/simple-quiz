2db136905412b300ff1fee95b004afd02649bd7d
Marianne_Rario <mariannecastillorario27@gmail.com>
2021-12-17 23:45:44 +0800

    add js animation

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let score = 0;
+  let x = 0;
   const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
   correctAns.forEach((ans, i) => {
     if (userAns[i] === ans) {
       score += 25;
     }
   });
+  scrollTo(0, 0);
   scoreDiv.classList.remove("d-none");
-  scoreSpan.textContent = score + "%";
+  const timer = setInterval(() => {
+    scoreSpan.textContent = `${x}%`;
+    if (x === score) {
+      return clearInterval(timer);
+    }
+    x++;
+  }, 20);
 });
